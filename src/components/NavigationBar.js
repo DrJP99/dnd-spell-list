4ab6fc140fdf65d084c3cde08158265c92f66cfd
Juan Pablo Gonzalez <43384928+DrJP99@users.noreply.github.com>
2023-07-23 18:15:24 -0700

    navbar collapse fix

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,30 +1,46 @@
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
 const NavigationBar = () => {
 	return (
-		<Navbar expand='lg' bg='danger' sticky='top' className='mb-4'>
+		<Navbar
+			expand="lg"
+			bg="danger"
+			sticky="top"
+			className="mb-4"
+			collapseOnSelect
+		>
 			<Container>
-				<Navbar.Brand href='/' as={Link} to={'/'}>
+				<Navbar.Brand href="/" as={Link} to={"/"}>
 					My Spell List
 				</Navbar.Brand>
-				<Navbar.Toggle aria-controls='basic-navbar-nav' />
-				<Navbar.Collapse id='basic-navbar-nav'>
-					<Nav className='me-auto'>
-						<Nav.Link href='/' as={Link} to='/'>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="me-auto">
+						<Nav.Link href="/" as={Link} to="/" eventKey={"/"}>
 							home
 						</Nav.Link>
-						<Nav.Link href='/character' as={Link} to='/character'>
+						<Nav.Link
+							href="/character"
+							as={Link}
+							to="/character"
+							eventKey={"/character"}
+						>
 							character
 						</Nav.Link>
-						<Nav.Link href='/profile' as={Link} to='/profile'>
+						<Nav.Link
+							href="/profile"
+							as={Link}
+							to="/profile"
+							eventKey={"/profile"}
+						>
 							profile
 						</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
 		</Navbar>
-	);
-};
+	)
+}
 
-export default NavigationBar;
+export default NavigationBar
